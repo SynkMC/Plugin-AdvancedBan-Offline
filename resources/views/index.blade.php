9e78dc933b65d311c183205dcb8a677038a71c98
Arnaud Lier <arnaud@eclixo.com>
2022-04-22 10:40:32 +0200

    deleted very bad code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,91 +3,96 @@
 @section('title', trans('advancedban::messages.title'))
 
 @section('content')
-    <div class="container content">
-    	<div class="row">
-	    	<div class="col">
-	    		<h1>{{ trans('advancedban::messages.title') }}</h1>
-	    	</div>
+    <div>
+        <div class="row">
+            <div class="col">
+                <h1>{{ trans('advancedban::messages.title') }}</h1>
+            </div>
 
-	    	<div class="col">
-			    <form class="mb-3 float-right" action="{{ route('advancedban.index') }}" method="GET">
-			        <div class="mb-2">
-			            <label for="searchInput" class="form-label sr-only">{{ trans('messages.actions.search') }}</label>
+            <div class="col">
+                <form class="mb-3 float-end" action="{{ route('advancedban.index') }}" method="GET">
+                    <div class="mb-2">
+                        <label for="searchInput" class="form-label sr-only">{{ trans('messages.actions.search') }}</label>
 
-			            <div class="input-group">
-			                <input type="text" class="form-control" id="searchInput" name="q" value="{{ request()->input('q') }}" placeholder="{{ trans('messages.actions.search') }}">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="searchInput" name="q"
+                                value="{{ request()->input('q') }}"
+                                placeholder="{{ trans('messages.actions.search') }}">
 
-				            <button type="submit" class="btn btn-primary">
-				                <i class="bi bi-search fa-sm"></i>
-				            </button>
-			            </div>
-			        </div>
-			    </form>
-			</div>
-		</div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="bi bi-search fa-sm"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
 
-		<div class="table-wrapper table-responsive">
-			<table class="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th scope="col">{{ trans('messages.fields.type') }}</th>
-						<th scope="col">{{ trans('advancedban::messages.username') }}</th>
-						<th scope="col">{{ trans('advancedban::messages.reason') }}</th>
-						<th scope="col">{{ trans('advancedban::messages.staff') }}</th>
-						<th scope="col">{{ trans('messages.fields.date') }}</th>
-						<th scope="col">{{ trans('advancedban::messages.expires_at') }}</th>
-						<th scope="col" class="text-right">{{ trans('messages.fields.status') }}</th>
-					</tr>
-				</thead>
-				<tbody>
-					@forelse ($allPunishments as $punishment)
-						<tr class="text-nowrap">
-							<td>{{ $punishment->punishmentType }}</td>
-							<td><img src="https://crafthead.net/avatar/{{ $punishment->uuid }}/30"> {{ $punishment->name }}</td>
-							<td>{{ $punishment->reason }}</td>
-							<td><img data-name="{{ $punishment->operator }}" src="https://crafthead.net/avatar/8667ba71-b85a-4004-af54-457a9734eed7/30"> {{ $punishment->operator }}</td>
-							<td>{{ format_date(Carbon\Carbon::createFromTimestampMs($punishment->start)) }} <span class="badge badge-info">{{ strftime('%H:%M', $punishment->start / 1000) }}</span></td>
-							<td>
-								@if ($punishment->end != -1)
-									{{ format_date(Carbon\Carbon::createFromTimestampMs($punishment->end)) }}
-									<span class="badge badge-info">{{ strftime('%H:%M', $punishment->end / 1000) }}</span>
-								@else
-									N/A
-								@endif
-							</td>
-							<td class="text-right">
-								@if (in_array($punishment, $punishments) && time() < $punishment->end)
-									{{ trans('advancedban::messages.active') }}
-								@else
-									{{ trans('advancedban::messages.finished') }}
-								@endif
-							</td>
-						</tr>
-					@empty
-						<tr>
-							<td colspan="7">{{ trans('advancedban::messages.no_punishments_found') }}</td>
-						</tr>
-					@endforelse
-				</tbody>
-			</table>
+        <div class="table-wrapper table-responsive">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">{{ trans('messages.fields.type') }}</th>
+                        <th scope="col">{{ trans('advancedban::messages.username') }}</th>
+                        <th scope="col">{{ trans('advancedban::messages.reason') }}</th>
+                        <th scope="col">{{ trans('advancedban::messages.staff') }}</th>
+                        <th scope="col">{{ trans('messages.fields.date') }}</th>
+                        <th scope="col">{{ trans('advancedban::messages.expires_at') }}</th>
+                        <th scope="col" class="text-right">{{ trans('messages.fields.status') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($allPunishments as $punishment)
+                        <tr class="text-nowrap">
+                            <td>{{ $punishment->punishmentType }}</td>
+                            <td><img src="https://crafthead.net/avatar/{{ $punishment->uuid }}/30">
+                                {{ $punishment->name }}</td>
+                            <td>{{ $punishment->reason }}</td>
+                            <td><img data-name="{{ $punishment->operator }}"
+                                    src="https://crafthead.net/avatar/8667ba71-b85a-4004-af54-457a9734eed7/30">
+                                {{ $punishment->operator }}</td>
+                            <td>{{ format_date(Carbon\Carbon::createFromTimestampMs($punishment->start)) }} <span
+                                    class="badge bg-info">{{ strftime('%H:%M', $punishment->start / 1000) }}</span></td>
+                            <td>
+                                @if ($punishment->end != -1)
+                                    {{ format_date(Carbon\Carbon::createFromTimestampMs($punishment->end)) }}
+                                    <span class="badge bg-info">{{ strftime('%H:%M', $punishment->end / 1000) }}</span>
+                                @else
+                                    N/A
+                                @endif
+                            </td>
+                            <td class="text-end">
+                                @if ($punishments->contains($punishment) && time() > $punishment->end / 1000)
+                                    {{ trans('advancedban::messages.active') }}
+                                @else
+                                    {{ trans('advancedban::messages.finished') }}
+                                @endif
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="7">{{ trans('advancedban::messages.no_punishments_found') }}</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
 
-			{{ $allPunishments->links() }}
-		</div>
-	</div>
+            {{ $allPunishments->links() }}
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
-	<script>
-		window.addEventListener('load', function() {
-			var links = document.getElementsByTagName("img");    
-			var linksArr = Array.from(links);
+    <script>
+        window.addEventListener('load', function() {
+            var links = document.querySelectorAll("img");
 
-			linksArr.forEach(function(element) {
-				let name = element.getAttribute('data-name');
-				if (name === null) return;
+            links.forEach(function(element) {
+                let name = element.getAttribute('data-name');
+                if (name === null) return;
 
-				element.setAttribute('src', 'https://crafthead.net/avatar/' + name + '/30');
-			});
-		})
-	</script>
+                element.setAttribute('src', 'https://crafthead.net/avatar/' + name + '/30');
+            });
+        })
+    </script>
 @endpush
--- src/Controllers/AdvancedBanHomeController.php
+++ src/Controllers/AdvancedBanHomeController.php
@@ -17,45 +17,63 @@ class AdvancedBanHomeController extends Controller
      */
     public function index(Request $request)
     {
-		if (config()->get('database.connections.advancedban') === null) {
-			abort_if(setting('advancedban.host') === null, 404);
+        if (config()->get('database.connections.advancedban') === null) {
+            abort_if(setting('advancedban.host') === null, 404);
 
-	        config()->set('database.connections.advancedban', [
-	            'driver'    => 'mysql',
-	            'host'      => setting('advancedban.host', '127.0.0.1'),
-	            'port'      => setting('advancedban.port', '3306'),
-	            'database'  => setting('advancedban.database', 'advancedban'),
-	            'username'  => setting('advancedban.username', 'advancedban'),
-	            'password'  => setting('advancedban.password'),
-	            'charset'   => 'utf8',
-	            'collation' => 'utf8_unicode_ci',
-	            'prefix'    => '',
-	            'strict'    => false
-	        ]);
-	    }
+            config()->set('database.connections.advancedban', [
+                'driver' => 'mysql',
+                'host' => setting('advancedban.host', '127.0.0.1'),
+                'port' => setting('advancedban.port', '3306'),
+                'database' => setting('advancedban.database', 'advancedban'),
+                'username' => setting('advancedban.username', 'advancedban'),
+                'password' => setting('advancedban.password'),
+                'charset' => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix' => '',
+                'strict' => false,
+            ]);
+        }
 
-	    $query = strtolower($request->input('q'));
+        $query = $request->input('q');
 
-	    $punishmentHistory = DB::connection('advancedban')->select('SELECT * FROM ' . setting('advancedban.historyTable', 'PunishmentHistory') . ' ORDER BY start DESC');
-	    $punishments = DB::connection('advancedban')->select('SELECT * FROM ' . setting('advancedban.punishmentTable', 'Punishments') . ' ORDER BY start DESC');
-	    
-	    $allPunishments = collect(array_merge($punishmentHistory, $punishments))->unique();
+        $punishmentHistory = DB::connection('advancedban')
+            ->table(setting('advancedban.historyTable', 'PunishmentHistory'))
+            ->where('name', 'LIKE', "%{$query}%")
+            ->orWhere('reason', 'LIKE', "%{$query}%")
+            ->orWhere('operator', 'LIKE', "%{$query}%")
+            ->orWhere('punishmentType', 'LIKE', "%{$query}%")
+            ->orderByDesc('start')
+            ->get();
+        $punishments = DB::connection('advancedban')
+            ->table(setting('advancedban.punishmentTable', 'Punishments'))
+            ->where('name', 'LIKE', "%{$query}%")
+            ->orWhere('reason', 'LIKE', "%{$query}%")
+            ->orWhere('operator', 'LIKE', "%{$query}%")
+            ->orWhere('punishmentType', 'LIKE', "%{$query}%")
+            ->orderByDesc('start')
+            ->get();
 
-	    if ($query) {
-	    	$allPunishments = $allPunishments->filter(function ($item) use ($query) {
-		    	return Str::contains(strtolower($item->name), $query) || 
-		    		Str::contains(strtolower($item->reason), $query) ||
-		    		Str::contains(strtolower($item->operator), $query) ||
-		    		Str::contains(strtolower($item->punishmentType), $query);
-	    	});
-	    }
+        $allPunishments = $punishmentHistory->merge($punishments)->unique();
 
-	    $currentPage = LengthAwarePaginator::resolveCurrentPage();
-	    $perPage = setting('advancedban.perPage', 10);
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = setting('advancedban.perPage', 10);
 
-	    $currentItems = $allPunishments->slice($perPage * ($currentPage - 1), $perPage);
-	    $paginator = new LengthAwarePaginator($currentItems, count($allPunishments), $perPage, $currentPage, ['path' => LengthAwarePaginator::resolveCurrentPath()]);
+        $currentItems = $allPunishments->slice(
+            $perPage * ($currentPage - 1),
+            $perPage
+        );
+        $paginator = new LengthAwarePaginator(
+            $currentItems,
+            count($allPunishments),
+            $perPage,
+            $currentPage,
+            ['path' => LengthAwarePaginator::resolveCurrentPath()]
+        );
 
-        return view('advancedban::index', ['punishments' => $punishments, 'punishmentHistory' => $punishmentHistory, 'allPunishments' => $paginator]);
+        return view('advancedban::index', [
+            'punishments' => $punishments,
+            'punishmentHistory' => $punishmentHistory,
+            'allPunishments' => $paginator,
+        ]);
     }
 }
